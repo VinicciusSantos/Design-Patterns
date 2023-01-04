6620dde20d6ac9bff4a42c364c82c5d406c991bc
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2023-01-04 11:58:36 -0300

    :sparkles: feat: template method

---
+++ src/behavioral/template_method/Invoice.ts
@@ -0,0 +1,17 @@
+import Item from "./Item";
+import TaxItem from "./TaxItem";
+
+export default class Invoice {
+    items: Item[] = []
+
+    public addItem(item: Item) {
+        this.items.push(item)
+    }
+
+    public getTaxes(): number {
+        let taxes = 0
+        for (const item of this.items) 
+        if (item instanceof TaxItem) taxes += item.getTax()
+        return taxes
+    }
+} 
---
+++ src/behavioral/template_method/Item.ts
@@ -0,0 +1,7 @@
+export default class Item {
+  constructor(
+    readonly category: string,
+    readonly description: string,
+    readonly price: number
+  ) {}
+}
